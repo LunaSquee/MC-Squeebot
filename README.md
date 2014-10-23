e789aae0ac99bfc13da38e50892e95a044e27317
LunaSquee <imsonotsleepy@gmail.com>
2014-10-23 23:43:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Node Minecraft Server Bot
+A cool bot that uses [node.js](http://nodejs.org/) and runs a minecraft server, adding more commands.
+
+### Setting up
+1. Clone this repository 
+2. Create a folder "server" and put a [minecraft server jar](https://minecraft.net/download) file into it (named `minecraft_server.jar`)
+3. Change memory allocations in `run.js`
+4. Start the server `node run.js`
---
+++ run.js
@@ -0,0 +1,138 @@
+#!/usr/bin/env node
+var exec = require('child_process'),
+    request = require('request'),
+    readline = require('readline'),
+    util = require('util'),
+    colors = require('colors'),
+    serverdir = __dirname+"/server",            // Minecraft server directory
+    server_process = null,                      // Server process
+    randomkey = true,                           // Generate a random key on each run
+    ramStart = 1024,                            // Ram start (-Xms[THIS]M)
+    ramMax = 1024,                              // Ram maximum (-Xmx[THIS]M)
+    jarname = "minecraft_server.jar",           // Name of the server jar
+    botname = "Squeebot";                       // Name of the bot
+    commandslist = ["!commands - All commands", "!np - Currently playing song"];
+    
+    function getCurrentSongData(callback) {
+        request({
+            url: "http://radio.djazz.se/icecast.php",
+            json: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                if(body.title != null) {
+                    var theTitle = new Buffer(body.title, "utf8").toString("utf8");
+                    var splitUp = theTitle.replace(/\&amp;/g, "&").split(" - ");
+                    if(splitUp.length===2) {
+                        theTitle=splitUp[1]+(splitUp[0]?" by "+splitUp[0]:"");
+                    }
+                    callback(theTitle, body.listeners, true);
+                } else {
+                    callback("Parasprite Radio is offline!", "", false);
+                }
+            } else {
+                callback("Parasprite Radio is offline!", "", false);
+            }
+        });
+    }
+    
+    function sendMessage(user, msg, color, type) {
+        var def = {text:msg, color:color}
+        switch(type) {
+            case 1:
+                def.text = "<%s> %s".format(botname, msg);
+                break;
+            case 2:
+                def.text = "* %s %s".format(botname, msg);
+                break;
+            case 3:
+                def.text = msg;
+                break;
+        }
+        if(server_process) {
+            server_process.stdin.write("tellraw "+user+" "+JSON.stringify(def)+"\r");
+        }
+    }
+    
+    function handleMessage(username, message, simplified) {
+        if(simplified[0]==="!np") {
+            getCurrentSongData(function(d, e, i) { if(i) { sendMessage("@a", "Now playing: "+d+" | Listeners: "+e, "white", 1);} else { sendMessage("@a", d, "white", 1)}});
+        } 
+        else if(simplified[0]==="!commands") {
+            sendMessage("@a", "--- Currently available commands for "+botname+" ---", "dark_green", 3);
+            commandslist.forEach(function(d) {
+                sendMessage("@a", d, "white", 3);
+            });
+            sendMessage("@a", "End of commands", "green", 3);
+        }
+        else if(simplified[0]==="!clear") {
+            server_process.stdin.write("weather clear\r");
+        }
+    }
+    
+    function processMessage(inp) {
+        mylog(inp);
+        var thing = inp.trim().match(/^\[(\d\d:\d\d:\d\d)\] \[([\w ]+)\/(\w+)\]: (.*)$/);
+        if(thing) {
+            var usrmsg = thing[4].match(/^<([^>]+)> (.*)/);
+            if(usrmsg) {
+                var simplified = usrmsg[2].replace(/\:/g, ' ').replace(/\,/g, ' ').replace(/\./g, ' ').replace(/\?/g, ' ').trim().split(' ');
+                handleMessage(usrmsg[1], usrmsg[2], simplified)
+            }
+        }
+    }
+    
+    server_process = exec.spawn(
+        "java",
+        ["-Xms"+ramStart+"M", "-Xmx"+ramMax+"M", "-jar", jarname, "nogui"],
+        { cwd:serverdir }
+    );
+    
+    server_process.stdout.on('data', function(data) {
+        data.toString().trim().split("\n").forEach(function(d) {
+            processMessage(d);
+        });
+    });
+    
+    server_process.stderr.on('data', function(data) {
+        data.toString().trim().split("\n").forEach(function(d) {
+            processMessage(d);
+        });
+    });
+    
+    server_process.on('exit', function(data) {
+        server_process = null;
+        process.exit(0);
+    });
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.setPrompt("");
+
+    rl.on('line', function (line) {
+        if (line === '') {
+            return;
+        } else {
+            if(server_process){
+                server_process.stdin.write(line+'\r');
+            }
+        }
+        rl.prompt(true);
+    });
+    
+    rl.setPrompt(util.format("> ".bold.magenta), 2);
+    
+    function mylog() {
+        // rl.pause();
+        rl.output.write('\x1b[2K\r');
+        console.log.apply(console, Array.prototype.slice.call(arguments));
+        // rl.resume();
+        rl._refreshLine();
+    }
+    
+    String.prototype.format = function(){
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(this.valueOf());
+      return util.format.apply(util, args);
+    };
